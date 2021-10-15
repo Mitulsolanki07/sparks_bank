5f9290d3d54a92a35699f64ee2dc8a912951c273
Mitulsolanki07 <72183889+Mitulsolanki07@users.noreply.github.com>
2021-10-15 14:57:57 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank Transaction | Home </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="/css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <header>
+        <h1 class="company-title">The Sparks Foundation</h1>
+    </header>
+    <!-- <main>
+<div class="d-grid gap-2 col-6 mx-auto">
+  <ul>
+      <li><a href="transfer_money.php"><button class="btn btn-primary" type="button">View All Users</button></a></li>
+      <li><a href="transfer_money.php"><button class="btn btn-primary" type="button">Transact Money</button></a></li>
+      <li><a href="transactionHistory.php"><button class="btn btn-primary" type="button">Transaction History</button></a></li>
+  </ul>
+</div>
+</main> -->
+
+<div class="bg">
+  <div class="center">
+    <ul>
+      <li class="operations"><a href="transfer_money.php"><button class="btn" id="red">View All Users</button></a></li>
+      <li class="operations"><a href="transfer_money.php"><button class="btn" id="blue">Trasnfer Money</button></a></li>
+      <li class="operations"><a href="transactionHistory.php"><button class="btn" id="green">View Transfer History</button></a></li>
+    </ul>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+</body>
+</html>
---
+++ transactionHistory.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="/css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+  
+</body>
+</html>
+	<div class="container">
+        <h2 class="text-center pt-4">Transaction History</h2>
+        
+       <br>
+       <div class="table-responsive-sm">
+    <table class="table table-hover table-striped table-condensed table-bordered">
+        <thead>
+            <tr>
+                <th class="text-center">S.No.</th>
+                <th class="text-center">Sender</th>
+                <th class="text-center">Receiver</th>
+                <th class="text-center">Amount</th>
+                <th class="text-center">Date & Time</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+
+            include 'config.php';
+
+            $sql ="select * from transaction";
+
+            $query =mysqli_query($conn, $sql);
+
+            while($rows = mysqli_fetch_assoc($query))
+            {
+        ?>
+
+            <tr>
+            <td class="py-2"><?php echo $rows['sno']; ?></td>
+            <td class="py-2"><?php echo $rows['sender']; ?></td>
+            <td class="py-2"><?php echo $rows['receiver']; ?></td>
+            <td class="py-2"><?php echo $rows['balance']; ?> </td>
+            <td class="py-2"><?php echo $rows['datetime']; ?> </td>
+                
+        <?php
+            }
+
+        ?>
+        </tbody>
+    </table>
+
+    </div>
+</div>
